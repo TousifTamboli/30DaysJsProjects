9c328148e28325340d4f9f7001696c80f37e7081
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-12-17 01:23:30 +0530

    day 2 js projects

--- QUIZ-APP/script.js
+++ QUIZ-APP/script.js
@@ -51,3 +51,10 @@ function startQuiz(){
     nextButton.innerHTML = "Next";
     showQuestion();
 }
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion
+    question;
+}
